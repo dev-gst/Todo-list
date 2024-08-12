fab7f90f2f1e2b3459f4664bd9773bc44e240a57
Gustavo Garcia <141775462+dev-gst@users.noreply.github.com>
2024-08-12 02:04:25 -0300

    feat: create TaskDTO

---
+++ src/Model/TaskDTO.java
@@ -0,0 +1,53 @@
+package Model;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class TaskDTO {
+
+    private final List<String> categories;
+    private final String name;
+    private final String description;
+    private final int priority;
+    private final TaskStatus status;
+    private final ZonedDateTime deadline;
+
+    public TaskDTO(String name, String description, int priority,
+                   TaskStatus status, ZonedDateTime deadline, List<String> categories) {
+        this.categories = categories;
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+        this.status = status;
+        this.deadline = deadline;
+    }
+
+    public TaskDTO(String name, String description, int priority,
+                   ZonedDateTime deadline, List<String> categories) {
+        this(name, description, priority, TaskStatus.TODO, deadline, categories);
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public ZonedDateTime getDeadline() {
+        return deadline;
+    }
+}
